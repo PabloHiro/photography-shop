6b36ba8b8ed810ae69d8e1b71431f7c204036ce2
PabloHiro <pablo.hiroshi.a.m@gmail.com>
2024-11-29 16:50:44 +0100

    feat: images and new layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,18 @@
-import './Header.css';
-import Contact from './Contact/Contact'
+import "./Header.css";
+import Contact from "./Contact/Contact";
 
 function Header({ title }) {
-    const reloadPage = () => {
-        window.location.reload();
-    };
-    return (
-        <div className='header'>
-            <div className='header-title' onClick={reloadPage}>
-                {title}
-            </div>
-            <Contact/>
-        </div>
-    );
+  const reloadPage = () => {
+    window.location.reload();
+  };
+  return (
+    <div className="header">
+      <div className="header-title" onClick={reloadPage}>
+        {title}
+      </div>
+      <Contact />
+    </div>
+  );
 }
 
 export default Header;
